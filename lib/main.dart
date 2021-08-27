bce748f301d13a55e966995c89fcec23519890cc
Gabriel Rocha <knuckles4x@gmail.com>
2021-08-27 11:18:37 -0300

    button section

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter layout demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.yellow,
       ),
       home: MyHomePage(title: 'Flutter layout demo'),
@@ -50,14 +41,14 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               padding: const EdgeInsets.only(bottom: 8),
               child: const Text(
-                'Oeschinen Lake Campground',
+                'Senhor Loro',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
             Text(
-              'Kandersteg, Switzerland',
+              'São Paulo, Brasil',
               style: TextStyle(
                 color: Colors.grey[500],
               ),
@@ -70,19 +61,22 @@ class _MyHomePageState extends State<MyHomePage> {
         Icons.star,
         color: Colors.red[500],
       ),
-      const Text('41'),
+      const Text('99'),
     ],
   ),
 );
 
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Widget buttonSection = Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      _buildButtonColumn(Colors.black, Icons.call, 'CALL'),
+      _buildButtonColumn(Colors.black, Icons.near_me, 'ROUTE'),
+      _buildButtonColumn(Colors.black, Icons.share, 'SHARE'),
+    ],
+  );
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -92,8 +86,32 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: [
            titleSection,
+           buttonSection,
          ],
 	    ),
     );
   }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  
 }
